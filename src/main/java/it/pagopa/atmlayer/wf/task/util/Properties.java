fa9f1356a381ac531641cc1fb764a4c0c7e12c20
SMANUM <125576365+SMANUM@users.noreply.github.com>
2023-10-20 14:34:41 +0200

    Cherry pick fix helm + fix for native

--- src/main/java/it/pagopa/atmlayer/wf/task/util/Properties.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/Properties.java
@@ -3,12 +3,14 @@ package it.pagopa.atmlayer.wf.task.util;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.config.spi.Converter;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithConverter;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 @ConfigMapping(prefix = "wf-task.config")
+@RegisterForReflection
 public interface Properties {
 
     @WithConverter(ConverterImplement.class)
