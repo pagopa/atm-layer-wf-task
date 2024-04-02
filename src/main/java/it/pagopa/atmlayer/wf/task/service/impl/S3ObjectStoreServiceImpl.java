221578e7733f89e4f28f2e216db68384d59e4a4d
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-02 13:32:19 +0200

    Tracing AWS S3 stream impl

--- src/main/java/it/pagopa/atmlayer/wf/task/service/impl/S3ObjectStoreServiceImpl.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/service/impl/S3ObjectStoreServiceImpl.java
@@ -1,20 +1,15 @@
 package it.pagopa.atmlayer.wf.task.service.impl;
 
-import io.smallrye.mutiny.Uni;
 import it.pagopa.atmlayer.wf.task.bean.exceptions.ErrorEnum;
 import it.pagopa.atmlayer.wf.task.bean.exceptions.ErrorException;
-import it.pagopa.atmlayer.wf.task.client.bean.ObjectStorePutResponse;
 import it.pagopa.atmlayer.wf.task.service.S3ObjectStoreService;
-import it.pagopa.atmlayer.wf.task.util.FileStorageS3Utils;
+import it.pagopa.atmlayer.wf.task.util.FileStorageS3Util;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
 
-import software.amazon.awssdk.core.async.AsyncRequestBody;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-
-import java.nio.charset.StandardCharsets;
+import java.io.ByteArrayInputStream;
 import java.util.Objects;
 
 @ApplicationScoped
@@ -22,34 +17,23 @@ import java.util.Objects;
 public class S3ObjectStoreServiceImpl implements S3ObjectStoreService {
 
     @Inject
-    S3AsyncClient s3;
-
-    @Inject
-    FileStorageS3Utils fileStorageS3Utils;
-
+    FileStorageS3Util fileStorageS3Util;
 
     public void writeLog(String message){
+
+        BlockingInputStreamAsyncRequestBody body = fileStorageS3Util.getBody();
+
         if (Objects.isNull(message)) {
-            String errorMessage = String.format("Aggiornamento file S3: message NULL %s non valido", message);
+            String errorMessage = String.format("message to save on S3: message %s non valido", message);
             log.error(errorMessage);
             throw new ErrorException(ErrorEnum.GENERIC_ERROR);
         }
-    
-        PutObjectRequest putObjectRequest = fileStorageS3Utils.buildPutRequest();
-        Uni.createFrom().future(() -> s3.putObject(putObjectRequest, AsyncRequestBody.fromString(message, StandardCharsets.UTF_8)))
-            .onFailure().transform(error -> {
-                String errorMessage = "Errore nel caricamento del file su S3";
-                log.error(errorMessage, error);
-                throw new ErrorException(ErrorEnum.GENERIC_ERROR);
-            })
-            .onItem().transformToUni(res -> {
-                log.info("Success uploading from s3");
-                return Uni.createFrom().item(ObjectStorePutResponse.builder().storageKey(putObjectRequest.key()).build());
-            })
-            .subscribe().with(
-                response -> {}, // Empty subscriber as we don't need to do anything with the response
-                throwable -> log.error("Error occurred while writing log", throwable)
-            );
+
+        log.info("string to upload: {}: length={}", message, message.length());
+
+        // Provide the stream of data to be uploaded.
+        body.writeInputStream(new ByteArrayInputStream(message.getBytes()));
+
     }
 
 }
---
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/FileStorageS3Util.java
@@ -0,0 +1,35 @@
+package it.pagopa.atmlayer.wf.task.util;
+
+import java.util.concurrent.CompletableFuture;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import lombok.Getter;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+@Getter
+@Singleton
+public class FileStorageS3Util {
+
+    @Inject
+    S3AsyncClient s3;
+
+    @Inject
+    ObjectStoreProperties objectStoreProperties;
+
+    BlockingInputStreamAsyncRequestBody body;
+
+    CompletableFuture<PutObjectResponse> responseFuture;
+
+    @PostConstruct
+    public void init() {
+        body = AsyncRequestBody.forBlockingInputStream(null);
+
+        responseFuture = s3.putObject(r -> r.bucket(objectStoreProperties.bucket().name())
+                .key(objectStoreProperties.resource().pathTemplate() + "/trace.log"), body);
+    }
+}
