d974fbd123c1e1d0999094d16222fcb1cd1dedce
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-02 17:51:35 +0200

    update tracing logic

--- src/main/java/it/pagopa/atmlayer/wf/task/util/FileStorageS3Util.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/FileStorageS3Util.java
@@ -1,7 +1,5 @@
 package it.pagopa.atmlayer.wf.task.util;
 
-import java.util.concurrent.CompletableFuture;
-
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -11,7 +9,6 @@ import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 @Getter
 @Singleton
@@ -33,8 +30,6 @@ public class FileStorageS3Util {
 
         body = AsyncRequestBody.forBlockingInputStream(null);
         
-        s3Client.putObject(r -> r.bucket(properties.bucket().name())
-                .key(properties.resource().pathTemplate() + "/trace.log"), body);
     }
 
     public void complete(){
