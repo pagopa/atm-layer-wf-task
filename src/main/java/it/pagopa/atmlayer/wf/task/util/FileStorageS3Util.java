8437208f7d2fec6ed9ce3ea2a9ed7756660073b0
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-03 15:51:46 +0200

    Use Multipart transfer on aws S3 for tracing purpose

--- src/main/java/it/pagopa/atmlayer/wf/task/util/FileStorageS3Util.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/FileStorageS3Util.java
@@ -1,17 +1,22 @@
 package it.pagopa.atmlayer.wf.task.util;
 
+import java.util.List;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
+import com.amazonaws.services.s3.model.PartETag;
+import com.amazonaws.services.s3.model.UploadPartRequest;
+import com.amazonaws.services.s3.model.UploadPartResult;
+
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import lombok.Getter;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.core.async.AsyncRequestBody;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.transfer.s3.S3TransferManager;
-import software.amazon.awssdk.transfer.s3.internal.TransferManagerFactory;
-import software.amazon.awssdk.transfer.s3.internal.TransferManagerFactory.DefaultBuilder;
-import software.amazon.awssdk.transfer.s3.model.UploadRequest;
+import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
 
 @Getter
 @Singleton
@@ -20,25 +25,34 @@ public class FileStorageS3Util {
     @Inject
     Properties properties;
 
-    S3AsyncClient s3Client;
+    BlockingInputStreamAsyncRequestBody body;
 
-    
+    AmazonS3 s3Client;
+
+    InitiateMultipartUploadRequest initRequest;
+
     @PostConstruct
     public void init() {
-        s3Client = S3AsyncClient.crtBuilder()
-            .credentialsProvider(DefaultCredentialsProvider.create())
-            .region(Region.of(properties.bucket().region()))
-            .build();
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(properties.bucket().region())
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
     }
 
-    public void appendS3(String message) {
-        S3TransferManager tm = TransferManagerFactory.createTransferManager(new DefaultBuilder().s3Client(s3Client));
-        UploadRequest uploadRequest = UploadRequest.builder()
-                                                        .requestBody(AsyncRequestBody.fromString(message))
-                                                        .putObjectRequest(req -> req.bucket(properties.bucket().name()).key(properties.resource().pathTemplate() + "/trace.log"))
-                                                        .build();
+    public InitiateMultipartUploadResult initMultipartUpload() {
+        initRequest = new InitiateMultipartUploadRequest(properties.bucket().name(), properties.resource().pathTemplate().concat("/trace.log"));
+        return s3Client.initiateMultipartUpload(initRequest);
+    }
 
-        tm.upload(uploadRequest);
+    public UploadPartResult uploadPart(UploadPartRequest uploadRequest){
+        // Upload the part
+        return s3Client.uploadPart(uploadRequest);
+    }
+
+    public void completeUpload(InitiateMultipartUploadResult initResponse, List<PartETag> partETags){
+        // Complete the multipart upload.
+        CompleteMultipartUploadRequest compRequest = new CompleteMultipartUploadRequest(properties.bucket().name(), properties.resource().pathTemplate().concat("/trace.log"), initResponse.getUploadId(), partETags);
+        s3Client.completeMultipartUpload(compRequest);
     }
 
 }
