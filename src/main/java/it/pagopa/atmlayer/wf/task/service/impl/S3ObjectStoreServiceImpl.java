1c21c0e29f84199cb11780260937eb9b0585a5e2
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-03 12:29:22 +0200

    Transfer manager S3

--- src/main/java/it/pagopa/atmlayer/wf/task/service/impl/S3ObjectStoreServiceImpl.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/service/impl/S3ObjectStoreServiceImpl.java
@@ -4,15 +4,12 @@ import it.pagopa.atmlayer.wf.task.bean.exceptions.ErrorEnum;
 import it.pagopa.atmlayer.wf.task.bean.exceptions.ErrorException;
 import it.pagopa.atmlayer.wf.task.service.S3ObjectStoreService;
 import it.pagopa.atmlayer.wf.task.util.FileStorageS3Util;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
-
-import java.io.ByteArrayInputStream;
 import java.util.Objects;
 
-@ApplicationScoped
+@Singleton
 @Slf4j
 public class S3ObjectStoreServiceImpl implements S3ObjectStoreService {
 
@@ -20,8 +17,6 @@ public class S3ObjectStoreServiceImpl implements S3ObjectStoreService {
     FileStorageS3Util fileStorageS3Util;
 
     public void writeLog(String message){
-
-        BlockingInputStreamAsyncRequestBody body = fileStorageS3Util.getBody();
 
         if (Objects.isNull(message)) {
             String errorMessage = String.format("message to save on S3: message %s non valido", message);
@@ -31,7 +26,7 @@ public class S3ObjectStoreServiceImpl implements S3ObjectStoreService {
 
         log.info("string to upload: {}: length={}", message, message.length());
 
-        body.writeInputStream(new ByteArrayInputStream(message.getBytes()));
+        fileStorageS3Util.appendS3(message);
 
     }
 
--- src/main/java/it/pagopa/atmlayer/wf/task/util/FileStorageS3Util.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/FileStorageS3Util.java
@@ -1,15 +1,17 @@
 package it.pagopa.atmlayer.wf.task.util;
 
-
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import lombok.Getter;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.async.AsyncRequestBody;
-import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.internal.TransferManagerFactory;
+import software.amazon.awssdk.transfer.s3.internal.TransferManagerFactory.DefaultBuilder;
+import software.amazon.awssdk.transfer.s3.model.UploadRequest;
 
 @Getter
 @Singleton
@@ -18,22 +20,25 @@ public class FileStorageS3Util {
     @Inject
     Properties properties;
 
-    BlockingInputStreamAsyncRequestBody body;
-
     S3AsyncClient s3Client;
 
+    
     @PostConstruct
     public void init() {
         s3Client = S3AsyncClient.crtBuilder()
             .credentialsProvider(DefaultCredentialsProvider.create())
             .region(Region.of(properties.bucket().region()))
             .build();
+    }
 
-        body = AsyncRequestBody.forBlockingInputStream(null);
+    public void appendS3(String message) {
+        S3TransferManager tm = TransferManagerFactory.createTransferManager(new DefaultBuilder().s3Client(s3Client));
+        UploadRequest uploadRequest = UploadRequest.builder()
+                                                        .requestBody(AsyncRequestBody.fromString(message))
+                                                        .putObjectRequest(req -> req.bucket(properties.bucket().name()).key(properties.resource().pathTemplate() + "/trace.log"))
+                                                        .build();
 
-        s3Client.putObject(r -> r.bucket(properties.bucket().name())
-                .key(properties.resource().pathTemplate() + "/trace.log"), body);
-        
+        tm.upload(uploadRequest);
     }
 
 }
