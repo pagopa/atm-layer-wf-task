8998f84c513680bf525e73612303dc85b008ab21
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-04 16:56:51 +0200

    Revert "Update logic"
    This reverts commit 290169b56340de8d383c0c743030d03d82eb9e1f.

--- src/main/java/it/pagopa/atmlayer/wf/task/util/Tracer.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/Tracer.java
@@ -6,9 +6,7 @@ import java.util.concurrent.TimeUnit;
 import io.quarkus.scheduler.Scheduled;
 import it.pagopa.atmlayer.wf.task.service.impl.S3ObjectStoreServiceImpl;
 import jakarta.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class Tracer {
     
     @Inject
@@ -20,15 +18,12 @@ public class Tracer {
     private static String message = new String();
 
     public static void trace(String msg){
-        log.info("msg: " + msg);
         message.concat(msg);
-        log.info("message: " + message);
     }
 
     @Scheduled(every = "30s", delay = 5, delayUnit = TimeUnit.SECONDS)
     public void tracerJob(){
         if (properties.isTraceLoggingEnabled()){
-            log.info("writing: " + message);
             objectStoreServiceImpl.writeLog(message);
             message = new String();
         }
