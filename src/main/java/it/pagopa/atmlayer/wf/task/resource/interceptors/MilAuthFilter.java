78dc15d79150f056e335517b48f9897df48a3c15
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-10 15:23:44 +0200

    Optimization

--- src/main/java/it/pagopa/atmlayer/wf/task/resource/interceptors/MilAuthFilter.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/resource/interceptors/MilAuthFilter.java
@@ -1,6 +1,7 @@
 package it.pagopa.atmlayer.wf.task.resource.interceptors;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.jboss.resteasy.reactive.client.spi.ResteasyReactiveClientRequestContext;
@@ -15,14 +16,14 @@ import jakarta.ws.rs.client.ClientResponseContext;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class MilAuthFilter implements ResteasyReactiveClientRequestFilter, ResteasyReactiveClientResponseFilter{
+public class MilAuthFilter implements ResteasyReactiveClientRequestFilter, ResteasyReactiveClientResponseFilter {
 
     @Inject
     Properties properties;
 
     @Override
-    public void filter(ResteasyReactiveClientRequestContext requestContext) {     
-        if (properties.isTraceLoggingEnabled()){
+    public void filter(ResteasyReactiveClientRequestContext requestContext) {
+        if (properties.isTraceLoggingEnabled()) {
             String transactionId = requestContext.getHeaderString("TransactionId");
             Tracer.trace(transactionId + " | ============== REQUEST MIL AUTH CLIENT ==============");
             Tracer.trace(transactionId + " | HEADERS: " + requestContext.getStringHeaders());
@@ -33,20 +34,23 @@ public class MilAuthFilter implements ResteasyReactiveClientRequestFilter, Reste
 
     @Override
     public void filter(ResteasyReactiveClientRequestContext requestContext, ClientResponseContext responseContext) {
-        if (properties.isTraceLoggingEnabled()){
+        if (properties.isTraceLoggingEnabled()) {
             String transactionId = requestContext.getHeaderString("TransactionId");
             Tracer.trace(transactionId + " | ============== RESPONSE MIL AUTH CLIENT ==============");
             Tracer.trace(transactionId + " | STATUS: " + responseContext.getStatus());
             Tracer.trace(transactionId + " | HEADERS: " + responseContext.getHeaders());
             if (responseContext.getEntityStream() != null) {
-                try (InputStream entityStream = responseContext.getEntityStream()) {
+                InputStream entityStream = responseContext.getEntityStream();
                     if (entityStream != null) {
-                      byte[] bytes = entityStream.readAllBytes();
-                      Tracer.trace(transactionId + " | BODY: " + new String(bytes));
+                        try {
+                            Tracer.trace(transactionId + " | BODY: " + new String(entityStream.readAllBytes()));
+                        } catch (IOException e) {
+                            Tracer.trace(transactionId + " | Error while tracing operation. . .");
+                        }
                     }
+                Tracer.trace(transactionId + " | ============== RESPONSE MIL AUTH CLIENT ==============");
             }
-            Tracer.trace(transactionId + " | ============== RESPONSE MIL AUTH CLIENT ==============");
         }
     }
-    
+
 }
