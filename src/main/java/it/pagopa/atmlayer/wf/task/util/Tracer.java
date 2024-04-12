9e1143372fea699f26499b8f3c62a507aca5f4f5
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-12 14:57:44 +0200

    DynamoDB  integration

--- src/main/java/it/pagopa/atmlayer/wf/task/util/Tracer.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/Tracer.java
@@ -2,38 +2,43 @@ package it.pagopa.atmlayer.wf.task.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-
-import org.jboss.resteasy.reactive.RestResponse;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.quarkus.scheduler.Scheduled;
-import it.pagopa.atmlayer.wf.task.bean.Device;
-import it.pagopa.atmlayer.wf.task.bean.State;
-import it.pagopa.atmlayer.wf.task.client.bean.TokenResponse;
+import it.pagopa.atmlayer.wf.task.database.service.ConfigurationAsyncServiceImpl;
+import it.pagopa.atmlayer.wf.task.database.service.contract.ConfigurationService;
 import it.pagopa.atmlayer.wf.task.service.impl.S3ObjectStoreServiceImpl;
 import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+
 
 @RegisterForReflection
+@Slf4j
 public class Tracer {
 
     @Inject
-    private Properties properties;
+    private S3ObjectStoreServiceImpl objectStoreServiceImpl;
 
     @Inject
-    private S3ObjectStoreServiceImpl objectStoreServiceImpl;
+    private ConfigurationAsyncServiceImpl configurationAsyncServiceImpl;
 
     private static StringBuilder messageBuilder = new StringBuilder();
 
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
+    public static Boolean isTraceLoggingEnabled = false;
+
     @Scheduled(every = "2m", delay = 5, delayUnit = TimeUnit.SECONDS)
     public void tracerJob() {
-        if (properties.isTraceLoggingEnabled() && messageBuilder.length() > 0) {
-            objectStoreServiceImpl.writeLog(messageBuilder.toString().replaceAll("\\{\\}", ""));
-            messageBuilder.setLength(0);
-        }
+        configurationAsyncServiceImpl.get(ConfigurationService.TRACING).subscribe().with(configuration -> {
+            isTraceLoggingEnabled = configuration.isEnabled();
+            log.info("isTraceLoggingEnabled: {}", isTraceLoggingEnabled);
+            if (isTraceLoggingEnabled && messageBuilder.length() > 0) {
+                objectStoreServiceImpl.writeLog(messageBuilder.toString().replaceAll("\\{\\}", ""));
+                messageBuilder.setLength(0);
+            }
+        });
     }
 
     public static void trace(String transactionId, String toLog) {
