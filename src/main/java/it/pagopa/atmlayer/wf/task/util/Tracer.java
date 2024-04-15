4bee50e42c1a286bae905fa59fd615a6319a67cf
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-15 12:45:40 +0200

    Revert "revert dynamo changes"
    This reverts commit cfc479a232798b077d4ff130230453e969c01d63.

--- src/main/java/it/pagopa/atmlayer/wf/task/util/Tracer.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/util/Tracer.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.quarkus.scheduler.Scheduled;
+import it.pagopa.atmlayer.wf.task.database.dynamo.service.ConfigurationAsyncServiceImpl;
+import it.pagopa.atmlayer.wf.task.database.dynamo.service.contract.ConfigurationService;
 import it.pagopa.atmlayer.wf.task.service.impl.S3ObjectStoreServiceImpl;
 import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,9 @@ public class Tracer {
     @Inject
     private S3ObjectStoreServiceImpl objectStoreServiceImpl;
 
+    @Inject
+    private ConfigurationAsyncServiceImpl configurationAsyncServiceImpl;
+
     private static StringBuilder messageBuilder = new StringBuilder();
 
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
@@ -24,14 +29,14 @@ public class Tracer {
 
     @Scheduled(every = "2m")
     public void tracerJob() {
-        /* configurationAsyncServiceImpl.get(ConfigurationService.TRACING).subscribe().with(configuration -> {
-            isTraceLoggingEnabled = configuration.getEnabled(); */
+        configurationAsyncServiceImpl.get(ConfigurationService.TRACING).subscribe().with(configuration -> {
+            isTraceLoggingEnabled = configuration.getEnabled();
             log.info("isTraceLoggingEnabled: {}", isTraceLoggingEnabled);
-            if (true && messageBuilder.length() > 0) {
+            if (isTraceLoggingEnabled && messageBuilder.length() > 0) {
                 objectStoreServiceImpl.writeLog(messageBuilder.toString().replaceAll("\\{\\}", ""));
                 messageBuilder.setLength(0);
             }
-        //});
+        });
     }
 
     public static void trace(String transactionId, String toLog) {
