0f7d318c6dbd4ea5d04d6fb4e515c60ee0d22680
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-19 14:52:29 +0200

    Refactor method latency

--- src/main/java/it/pagopa/atmlayer/wf/task/logging/latency/LatencyTracer.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/logging/latency/LatencyTracer.java
@@ -1,11 +1,14 @@
 package it.pagopa.atmlayer.wf.task.logging.latency;
 
+import io.quarkus.logging.Log;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.smallrye.mutiny.Uni;
 import it.pagopa.atmlayer.wf.task.logging.latency.producer.CloudWatchLogsProducer;
 import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsAsyncClient;
 
+@Slf4j
 @RegisterForReflection
 public class LatencyTracer extends CloudWatchLogsProducer {
 
@@ -25,9 +28,14 @@ public class LatencyTracer extends CloudWatchLogsProducer {
      * @param start - the start time, when the execution is started
      */
     public static void logElapsedTime(String label, String communicationType, long start) {
-        messageBuilder.append(label).append(" - Latency ").append(communicationType).append(" - Elapsed time [ms] = ").append(System.currentTimeMillis() - start);
+        messageBuilder.append(label).append(" - Latency ").append(communicationType).append(" - Elapsed time [ms] = ")
+                .append(System.currentTimeMillis() - start);
         Uni.createFrom().completionStage(
-                client.putLogEvents(generatePutLogEventRequest(GROUP_NAME, STREAM_NAME, messageBuilder.toString())));
+                client.putLogEvents(generatePutLogEventRequest(GROUP_NAME, STREAM_NAME, messageBuilder.toString())))
+                .onFailure().invoke(throwable -> {
+                    log.error("Error writing latency log on Cloudwatch: ", throwable);
+                })
+                .subscribe();
         messageBuilder.setLength(0);
     }
 
