08c358c86a1b9adc5d0cac49fe4a0d78a3789a81
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-19 15:19:50 +0200

    Update cloudwatch producer

--- src/main/java/it/pagopa/atmlayer/wf/task/logging/latency/producer/CloudWatchLogsProducer.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/logging/latency/producer/CloudWatchLogsProducer.java
@@ -3,11 +3,9 @@ package it.pagopa.atmlayer.wf.task.logging.latency.producer;
 
 import java.time.Instant;
 
-import io.quarkus.arc.profile.UnlessBuildProfile;
 import software.amazon.awssdk.services.cloudwatchlogs.model.InputLogEvent;
 import software.amazon.awssdk.services.cloudwatchlogs.model.PutLogEventsRequest;
 
-@UnlessBuildProfile(anyOf = { "native" })
 public abstract class CloudWatchLogsProducer {
 
     protected static PutLogEventsRequest generatePutLogEventRequest(String groupName, String streamName, String logMessage) {
