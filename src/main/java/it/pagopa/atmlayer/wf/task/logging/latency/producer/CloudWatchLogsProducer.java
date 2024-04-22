0f667ffb1f2fb483aa66ad125549c0bdbe087324
pasquales <pasqualesansonna@aurigaspa.com>
2024-04-22 13:25:18 +0200

    Added management realTracer and noopTracer for latency

--- src/main/java/it/pagopa/atmlayer/wf/task/logging/latency/producer/CloudWatchLogsProducer.java
+++ src/main/java/it/pagopa/atmlayer/wf/task/logging/latency/producer/CloudWatchLogsProducer.java
@@ -3,10 +3,11 @@ package it.pagopa.atmlayer.wf.task.logging.latency.producer;
 
 import java.time.Instant;
 
+import it.pagopa.atmlayer.wf.task.logging.latency.Tracer;
 import software.amazon.awssdk.services.cloudwatchlogs.model.InputLogEvent;
 import software.amazon.awssdk.services.cloudwatchlogs.model.PutLogEventsRequest;
 
-public abstract class CloudWatchLogsProducer {
+public abstract class CloudWatchLogsProducer extends Tracer {
 
     protected static PutLogEventsRequest generatePutLogEventRequest(String groupName, String streamName, String logMessage) {
         return PutLogEventsRequest.builder().logGroupName(groupName).logStreamName(streamName)
